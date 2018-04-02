23226097ae059b8798c3e6721ff6e0a9d2fbf4f8
Maurits van der Schee <maurits@vdschee.nl>
2018-04-02 15:13:48 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -2,9 +2,20 @@
 if [ ! -e target ]; then
     mkdir target
 fi
+if [ -e target/api.txt ]; then
+    rm target/api.txt
+fi
 if [ -e target/api.php ]; then
     rm target/api.php
 fi
-echo "<?php" > target/api.php
-echo 'namespace Com\Tqdev\CrudApi;' >> target/api.php
-find . -iname '*.php' -exec cat {} \; -exec echo \; | grep -v "^<?php\|^namespace \|^use \|spl_autoload_register\|//" >> target/api.php
+echo "<?php" > target/api.txt
+tee -a target/api.txt <<'EOF'
+/**
+ * PHP-CRUD-API                 License: MIT
+ * Maurits van der Schee: maurits@vdschee.nl
+ * https://github.com/mevdschee/php-crud-api
+ **/
+EOF
+echo 'namespace Com\Tqdev\CrudApi;' >> target/api.txt
+find . -iname '*.php' -exec cat {} \; -exec echo \; | grep -v "^<?php\|^namespace \|^use \|spl_autoload_register\|//" >> target/api.txt
+mv target/api.txt target/api.php
