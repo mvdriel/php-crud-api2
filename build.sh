a257fabc32e43f17fe40a642b1f5da7b9841ba7b
Maurits van der Schee <maurits@vdschee.nl>
2018-04-08 23:34:22 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+START=$(date +%s.%N)
 if [ ! -e target ]; then
     mkdir target
-fi
-if [ -e target/api.txt ]; then
-    rm target/api.txt
 fi
 if [ -e target/api.php ]; then
     rm target/api.php
 fi
-echo "<?php" > target/api.txt
-tee -a target/api.txt <<'EOF'
+echo "<?php" > target/api.php
+tee -a target/api.php >/dev/null <<'EOF'
 /**
  * PHP-CRUD-API                 License: MIT
  * Maurits van der Schee: maurits@vdschee.nl
  * https://github.com/mevdschee/php-crud-api
  **/
 EOF
-echo 'namespace Com\Tqdev\CrudApi;' >> target/api.txt
-find . -iname '*.php' -exec cat {} \; -exec echo \; | grep -v "^<?php\|^namespace \|^use \|spl_autoload_register\|^\s*//" >> target/api.txt
-mv target/api.txt target/api.php
+echo 'namespace Com\Tqdev\CrudApi;' >> target/api.php
+FILECOUNT=`find . -path ./tests -prune -o -path ./target -prune -o -iname '*.php' | grep '.php$' | wc -l`
+find . -path ./tests -prune -o -path ./target -prune -o -iname '*.php' -exec cat {} \; -exec echo \; | grep -v "^<?php\|^namespace \|^use \|spl_autoload_register\|^\s*//" >> target/api.php
+php -l target/api.php
+END=$(date +%s.%N)
+DIFF=$(echo "( $END - $START ) * 1000 / 1" | bc)
+echo "$FILECOUNT files combined in $DIFF ms"
