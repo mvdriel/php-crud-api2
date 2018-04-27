941ef67c4830be7d16ea754c4acc1496a8463bc8
Maurits van der Schee <maurits@vdschee.nl>
2018-04-27 21:51:12 +0200

    Add TreeMap

---
+++ src/Com/Tqdev/CrudApi/Api/TreeMap.php
@@ -0,0 +1,27 @@
+<?php
+namespace Com\Tqdev\CrudApi\Api;
+
+class TreeMap {
+
+	protected $branches = array();
+
+    public function put(array $path) {
+        if (count($path)==0) {
+            return;
+        }
+        $key = array_shift($path);
+        if (!isset($this->branches[$key])) {
+            $this->branches[$key] = new TreeMap();
+        }
+        $branches = $this->branches[$key];
+        $branches->put($path);
+	}
+
+    public function getKeys(): array {
+        return array_keys($this->branches);
+    }
+
+    public function get($key): PathTree {
+        return $this->branches[$key];
+    }
+}
