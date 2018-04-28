9ef7ba49b3e7b14443110ea9473869986d1e2619
Maurits van der Schee <maurits@vdschee.nl>
2018-04-28 08:45:11 +0200

    php_fmt

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-use Com\Tqdev\CrudApi\Config;
 use Com\Tqdev\CrudApi\Api;
+use Com\Tqdev\CrudApi\Config;
 use Com\Tqdev\CrudApi\Request;
 
 // do not reformat the following line
-spl_autoload_register(function ($class) { include str_replace('\\',DIRECTORY_SEPARATOR,"src\\$class.php"); });
+spl_autoload_register(function ($class) {include str_replace('\\', DIRECTORY_SEPARATOR, "src\\$class.php");});
 // as it is excluded in the build
 
 $config = new Config([
-    'database'=>'php-crud-api',
-    'username'=>'php-crud-api',
-    'password'=>'php-crud-api',
+    'database' => 'php-crud-api',
+    'username' => 'php-crud-api',
+    'password' => 'php-crud-api',
 ]);
-$request = new Request('GET','/posts/1');
+$request = new Request('GET', '/posts/1');
 $request->addHeader('Origin');
 $api = new Api($config);
 $response = $api->handle($request);
