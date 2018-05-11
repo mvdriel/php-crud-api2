3410c781956edcc030f7aacbb13cd0c2f9857483
Maurits van der Schee <maurits@vdschee.nl>
2018-05-11 07:04:23 +0200

    Fix build

--- src/Com/Tqdev/CrudApi/Cache/MemcachedCache.php
+++ src/Com/Tqdev/CrudApi/Cache/MemcachedCache.php
@@ -1,15 +1,52 @@
 <?php
 namespace Com\Tqdev\CrudApi\Cache;
 
-class MemcachedCache extends MemcacheCache
+class MemcachedCache implements Cache
 {
+    const PREFIX = 'phpcrudapi-';
+
+    protected $prefix;
+    protected $memcache;
+
+    public function __construct(String $config)
+    {
+        $this->init($config);
+    }
+
     protected function create(): object
     {
         return new \Memcached();
     }
 
+    private function init(String $config): void
+    {
+        if ($config == '') {
+            $address = 'localhost';
+            $port = 11211;
+        } elseif (strpos($config, ':') === false) {
+            $address = $config;
+            $port = 11211;
+        } else {
+            list($address, $port) = explode(':', $config);
+        }
+        $id = substr(md5(__FILE__), 0, 8);
+        $this->prefix = self::PREFIX . $id . '-';
+        $this->memcache = $this->create();
+        $this->memcache->addServer($address, $port);
+    }
+
     public function set(String $key, $value, int $ttl = 0): bool
     {
         return $this->memcache->set($this->prefix . $key, $value, $ttl);
+    }
+
+    public function get(String $key, bool $stale = false) /*: ?object*/
+    {
+        return $this->memcache->get($this->prefix . $key) ?: null;
+    }
+
+    public function clear(): bool
+    {
+        return $this->memcache->flush();
     }
 }
