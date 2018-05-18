52e25457a44f66f3bcef44a9fa1a808ac0413b8e
Maurits van der Schee <maurits@vdschee.nl>
2018-05-19 01:03:03 +0200

    bugfix

--- src/Com/Tqdev/CrudApi/Database/GenericDB.php
+++ src/Com/Tqdev/CrudApi/Database/GenericDB.php
@@ -105,7 +105,7 @@ class GenericDB
                 $sql .= ' RETURNING ' . $outputColumn;
                 break;
             case 'sqlsrv':
-                $sql = str_replace(') VALUES (', ') OUTPUT INSERTED.' . $outputColumn . ' VALUES (', $sql, 1);
+                $sql = str_replace(') VALUES (', ') OUTPUT INSERTED.' . $outputColumn . ' VALUES (', $sql);
                 break;
         }
         $stmt = $this->query($sql, $parameters);
